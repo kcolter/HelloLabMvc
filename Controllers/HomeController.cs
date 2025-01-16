5c115e0033ce59c6f3192570a8fe1cb1e44a2921
Keegan <62590150+kcolter@users.noreply.github.com>
2025-01-16 16:06:44 -0400

    Model passing to view, lab done

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,27 +6,19 @@ namespace HelloLabMvc.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
 
         public IActionResult Index()
         {
-            return View();
+            Person p1 = new Person();
+            p1.FirstName = "John";
+            p1.LastName = "Doe";
+
+            return View(p1);
         }
 
         public IActionResult Privacy()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace HelloLabMvc.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
